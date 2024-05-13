cdd9f00316f568ddbae4ff6ba704b8a203b237c6
PraveenGops <153624883+PraveenGops@users.noreply.github.com>
2024-05-13 22:35:05 +0530

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,4 +4,6 @@ echo "building the docker image"
 docker build -t test_image .
 echo "docker images"
 docker images
-
+echo "giving a tag"
+docker tag test_image praveengdocker/dev_region:v1
+docker push praveengdocker/dev_region:v1
