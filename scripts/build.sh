bc9ad96d5afebed9f0e85b20cb58bec5856f09fb
PraveenGops <153624883+PraveenGops@users.noreply.github.com>
2024-05-14 13:00:35 +0530

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,5 +7,6 @@ docker images
 echo "giving a tag"
 docker tag test_image praveengdocker/dev_region:jenkins
 docker images
+docker login -u praveengdocker -p G@7160598
 echo "pushing to repo"
 docker push praveengdocker/dev_region:jenkins
