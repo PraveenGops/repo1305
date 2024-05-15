11d8d2e9183beb0dc8587df05ccae395063cd9c5
PraveenGops <153624883+PraveenGops@users.noreply.github.com>
2024-05-15 13:07:31 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,6 +4,6 @@ echo "building image from docker-compose-file"
 docker-compose build
 docker images
 echo "pushing the image to hub"
-docker tag project1-website praveengdocker/dev_region:test1
+docker tag project1_website praveengdocker/dev_region:test1
 docker login -u praveengdocker -p G@7160598
 docker push praveengdocker/dev_region:test1
